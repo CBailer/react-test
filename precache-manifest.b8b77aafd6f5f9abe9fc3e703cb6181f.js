ec6d31f4c7a9c6bcfb82d755d70a9b054a30c641
cbailer <codybailer@gmail.com>
2019-01-31 15:22:18 -0500

    Updates

---
+++ precache-manifest.b8b77aafd6f5f9abe9fc3e703cb6181f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dc0373c62d803437060b",
+    "url": "/static/js/main.dc0373c6.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "dc0373c62d803437060b",
+    "url": "/static/css/main.4c7041c2.chunk.css"
+  },
+  {
+    "revision": "469995c38fcf81efbfffc1c4f9b670ce",
+    "url": "/index.html"
+  }
+];
